3511627706f2969bd9f539e4dd617972f2e86e73
Fausto Bencosme <argenijam@gmail.com>
2017-04-25 12:37:45 -0400

    Adding some useful rx operators

--- Source/Extensions.cs
+++ Source/Extensions.cs
@@ -1,13 +1,56 @@
 ﻿using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
 
 namespace InnovifySample
 {
+  using static DateTime;
+  using static Observable;
+
   public static class Extensions
   {
     public static IDisposable AddTo(this IDisposable disposable, CompositeDisposable disposables) {
       disposables.Add(disposable);
       return disposable;
     }
+
+    public static IObservable<Result> ComposeLatest<Source, Sample, Result>(
+     this IObservable<Source> source,
+     IObservable<Sample> sample,
+     Func<Source, Sample, Result> selector) =>
+     Defer(() =>
+     {
+       var lastIndex = -1;
+       return source.CombineLatest(
+           sample.Select(Tuple.Create<Sample, int>),
+           (a, b) =>
+           new
+           {
+             Index = b.Item2,
+             Sample = b.Item1,
+             Source = a
+           })
+         .Where(x => x.Index != lastIndex)
+         .Do(x => lastIndex = x.Index)
+         .Select(x => selector(x.Source, x.Sample));
+     });
+
+    public static IObservable<T> Debounce<T>(this IObservable<T> source, TimeSpan span)
+    {
+      var lastSent = Now - span;
+      return source
+        .Where(t =>
+        {
+          var valid = (Now - lastSent) >= span;
+          if (valid)
+            lastSent = Now;
+          return valid;
+        });
+    }
+
+    public static IObservable<Source> SampleLatest<Source, Sample>(
+        this IObservable<Source> source,
+        IObservable<Sample> sample) =>
+       source.ComposeLatest(sample, (p, _) => p);
   }
 }
