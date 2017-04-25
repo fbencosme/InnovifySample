b96d8add7f3319559d58d8d25c368b3f8c6a18cf
Edwin Ferreira <eferreira@kitsd.com>
2017-04-25 11:37:34 -0400

    Setting up shared code

---
+++ Source/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Disposables;
+
+namespace InnovifySample
+{
+  public static class Extensions
+  {
+    public static IDisposable AddTo(this IDisposable disposable, CompositeDisposable disposables) {
+      disposables.Add(disposable);
+      return disposable;
+    }
+  }
+}
