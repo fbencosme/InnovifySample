cc5adf21778f70ab0c3c47c538b95e4be9ce68ae
Fausto Bencosme <argenijam@gmail.com>
2017-04-25 12:53:45 -0400

    Contact form  basic validation.

--- Droid/Source/Fragments.cs
+++ Droid/Source/Fragments.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 
 using Android.App;
@@ -11,10 +12,12 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
-using System.Reactive.Linq;
 
 namespace InnovifySample.Droid
 {
+  /*
+   * Contact Form View.
+   */
   public class Contact : BaseFragment
   {
     protected override int LayoutRes => Resource.Layout.Contact;
@@ -28,6 +31,7 @@ namespace InnovifySample.Droid
       var website  = view.FindViewById<EditText>(Resource.Id.website);
       var position = view.FindViewById<EditText>(Resource.Id.position);
       var message  = view.FindViewById<EditText>(Resource.Id.message);
+      var contact  = view.FindViewById<Button>(Resource.Id.contactUs);
 
       Observable.CombineLatest(
         name    .RxTextChanged().StartWith(string.Empty),
@@ -35,24 +39,77 @@ namespace InnovifySample.Droid
         phone   .RxTextChanged().StartWith(string.Empty),
         website .RxTextChanged().StartWith(string.Empty),
         position.RxTextChanged().StartWith(string.Empty),
-        message .RxTextChanged().StartWith(string.Empty),
-        Tuple.Create);
+        message .RxTextChanged().StartWith(string.Empty)
+        , Tuple.Create)
+      .SampleLatest(contact.RxClick())
+       .Do(_ =>
+         OnClean(name, email, phone, website, position, message))
+      .SelectMany(_ =>
+        OnValidate(
+          Tuple.Create(name, _.Item1),
+          Tuple.Create(email, _.Item2),
+          Tuple.Create(phone, _.Item3),
+          Tuple.Create(website, _.Item4),
+          Tuple.Create(position, _.Item5),
+          Tuple.Create(message, _.Item6))
+     );
 
     }
 
-    void OnValidate(
+    void OnClean(
+     EditText name,
+     EditText email,
+     EditText phone,
+     EditText website,
+     EditText position,
+     EditText message)
+    {
+      name.Error     =
+      email.Error    =
+      phone.Error    =
+      website.Error  =
+      position.Error =
+      message.Error  = null;
+    }
+
+    IObservable<ContactInfo> OnValidate(
       Tuple<EditText, string> name,
       Tuple<EditText, string> email,
       Tuple<EditText, string> phone,
       Tuple<EditText, string> website,
       Tuple<EditText, string> position,
-      Tuple<EditText, string> message) {
+      Tuple<EditText, string> message) =>
 
+        OnValidate(name)     ||
+        OnValidate(email)    ||
+        OnValidate(phone)    ||
+        OnValidate(website)  ||
+        OnValidate(position) ||
+        OnValidate(message)
+    
+        ? Observable.Empty<ContactInfo>()
+        : Observable.Return(new ContactInfo { 
+          name     = name.Item2,
+          email    = email.Item2,
+          phone    = phone.Item2,
+          website  = website.Item2,
+          position = position.Item2,
+          message  = message.Item2
+        });
+
+    bool OnValidate(Tuple<EditText, string> t) {
+      var invalid = string.IsNullOrEmpty(t.Item2);
+      if (invalid)
+        t.Item1.Error = GetString(Resource.String.required);
+      return invalid;
     }
 
   }
 
-	public class Welcome : BaseFragment
+  /*
+   *  Welcome or first displayd View.
+   */
+  public class Welcome : BaseFragment
 	{
     protected override int LayoutRes => Resource.Layout.Welcome;
 
@@ -68,6 +125,9 @@ namespace InnovifySample.Droid
 
 	} 
 
+  /*
+   *  Bye or last step View.
+   */
   public class Bye : BaseFragment
   {
     protected override int LayoutRes => Resource.Layout.Bye;
--- Source/API.cs
+++ Source/API.cs
@@ -23,11 +23,13 @@ namespace InnovifySample
     }
   }
 
-  public class SignUp 
+ public class ContactInfo
   {
-    public string firstName { get; set; }
-    public string lastName  { get; set; }
-    public string email     { get; set; }
-    public string password  { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string phone { get; set; }
+    public string website { get; set; }
+    public string position { get; set; }
+    public string message { get; set; }
   }
 }
